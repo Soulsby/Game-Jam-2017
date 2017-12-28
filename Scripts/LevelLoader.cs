faf95a7a3e460d38ef7243c2fa4454149d1a10e4
Soulsby <jwsoulsby@gmail.com>
2017-12-28 00:40:35 -0600

    LevelLoader

--- Scripts/LevelLoader.cs
+++ Scripts/LevelLoader.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static LevelLoader loader;
+    public int nextLevel;
+
+    public void loadNextLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        loadNextLevel();
+    }
+
+
+
 }
