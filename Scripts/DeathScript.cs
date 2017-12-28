5ec7e3c148c6c2422ab0ae44cf60951bba9faf80
gasmask866 <34847354+gasmask866@users.noreply.github.com>
2017-12-28 01:37:07 -0500

    Push near the end

---
+++ Scripts/DeathScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScript : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    public void Ondeath()
+    { 
+        SceneManager.LoadScene("Scene1");
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
